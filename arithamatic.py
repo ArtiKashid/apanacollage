4b1e51d3f605bc265fec0113dfae9e5aefb74a07
ArtiKashid <98664015+ArtiKashid@users.noreply.github.com>
2022-07-11 16:14:56 +0530

    Add files via upload

---
+++ arithamatic.py
@@ -0,0 +1,44 @@
+def Add(iNum1, iNum2):
+
+    iSum = 0
+
+    # Perform calculation
+    iSum = iNum1 + iNum2
+
+    return iSum
+
+# ================================
+# Sub : substraction of given numbers
+# ================================
+def Sub(iNum1, iNum2):
+
+    iSum = 0
+
+    # Perform calculation
+    iSum = iNum1 - iNum2
+
+    return iSum
+
+# ================================
+# Mult : multiplicatin of given numbers
+# ================================
+def Mult(iNum1, iNum2):
+
+    iSum = 0
+
+    # Perform calculation
+    iSum = iNum1 * iNum2
+
+    return iSum
+
+# ================================
+# Div : division of given numbers
+# ================================
+def Div(iNum1, iNum2):
+
+    iSum = 0
+
+    # Perform calculation
+    iSum = iNum1 / iNum2
+
+    return iSum

b3ad57e4e0c1a6996cc6dd415f5db2332c3c5d6a
ArtiKashid <98664015+ArtiKashid@users.noreply.github.com>
2022-07-16 12:32:34 +0530

    Use of Enumerate

---
+++ Enumerate.py
@@ -0,0 +1,8 @@
+'''
+The Python enumerate() function adds a counter to an iterable object.
+enumerate() function can accept sequential indexes starting from zero
+'''
+subjects=('hindi','english','marathi','maths')
+for  i ,subj in enumerate(subjects):
+    print(i,subj)
+

8b087979c7ba3fa2d7282cffdbe6afaf2d101bfd
ArtiKashid <98664015+ArtiKashid@users.noreply.github.com>
2022-07-16 14:30:56 +0530

    Print Table of Number

---
+++ table.py
@@ -0,0 +1,9 @@
+#############################################################
+#
+#Write a Python Program to Print Table of a Given Number
+#
+#############################################################
+n=int(input("Enter the number:"))
+for i in range(1,11):
+
+    print(n, "x", i ,"=" ,n*i)

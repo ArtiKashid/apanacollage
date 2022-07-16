57b29274c117bfb77d0c8fb4b6388bd602244f1a
ArtiKashid <98664015+ArtiKashid@users.noreply.github.com>
2022-07-16 12:36:01 +0530

    How do you Concatenate Strings in Python

---
+++ ConcateString.py
@@ -0,0 +1,12 @@
+#######################################################
+# How do you Concatenate Strings in Python?
+# ->***We can use ‘+’ to concatenate strings.
+#######################################################
+var1= "python"
+var2= "java"
+var3= "is"
+var4= "coding"
+var5="languages"
+
+var6= ' '+var1 +' '+ var2 +' '+var3 + ' '+var4+ ' '+var5
+print(var6)
